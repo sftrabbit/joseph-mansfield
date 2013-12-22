ac928d4dd95c14298dabbcbe47c87de0d7a82457
Joseph Mansfield <sftrabbit@gmail.com>
2013-12-22 23:44:18 +0000

    Add basic template expansion algorithm
    Add a basic template expansion algorithm that repeatedly looks for
    template includes and then data placeholders, replacing them with the
    appropriate content until none are left to replace. Template includes are
    recursively processed.

---
+++ build.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import os
+import re
+import functools
+
+def get_template_file_name(template_name):
+	return "templates/" + template_name + ".htmt"
+
+def get_data_value(data, match):
+	data_name = match.group(2)
+	if data_name in data:
+		return data[match.group(2)]
+	return match.group(0)
+
+def get_template_content(data, match):
+	try:
+		template_name = match.group(2)
+		return expand_template(template_name, data).rstrip()
+	except IOError:
+		print("Template " + template_name + " does not exist")
+		return match.group(0)
+
+def expand_template(template_name, data):
+	with open(get_template_file_name(template_name)) as template_file:
+		template_content = template_file.read()
+
+		while True:
+			previous_content = template_content
+
+			template_content_func = functools.partial(get_template_content, data)
+			template_content = re.sub(r"@({)?(\w+)(?(1)})", template_content_func, template_content)
+
+			data_value_func = functools.partial(get_data_value, data)
+			template_content = re.sub(r"\$({)?(\w+)(?(1)})", data_value_func, template_content)
+
+			if template_content == previous_content:
+				break
+
+		return template_content
+
+def build():
+	project_directory = os.path.dirname(os.path.realpath(__file__));
+	os.chdir(project_directory)
+	print(expand_template("root", {'page': 'home'}))
+
+if __name__ == "__main__":
+	build()
