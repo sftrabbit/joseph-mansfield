13c66f440b9aad4f2490563699057d51f5bd0d01
Joseph Mansfield <sftrabbit@gmail.com>
2013-12-23 15:46:47 +0000

    Add build script that compiles styles
    Add a build script that generates the HTML pages and then compiles
    stylesheets using Compass. Because Compass doesn't provide a nice way to
    compile into an arbitrary build directory, the Compass configuration is
    generated at compilation into the build directory and Compass is run
    from there. `compass watch` can be used from the build directory once
    the config file exists.

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+BUILD_DIR=.
+PROJECT_DIR=$(dirname $0)
+
+echo "Compiling HTML pages"
+${PROJECT_DIR}/compile.py
+
+BUILD_DIR_REGEX="s/\${BUILD_DIR}/${BUILD_DIR}/"
+PROJECT_DIR_REGEX="s/\${PROJECT_DIR}/${PROJECT_DIR}/"
+CONFIG_FILE_IN=${PROJECT_DIR}/config.rb.in
+CONFIG_FILE=${BUILD_DIR}/config.rb
+sed -e $BUILD_DIR_REGEX -e $PROJECT_DIR_REGEX $CONFIG_FILE_IN > $CONFIG_FILE
+
+echo "Compiling compass stylesheets"
+compass compile
